936cdd651458305660d675f75e40d1b6c4dce91e
Marcus Tiffen <marcustiffen6@gmail.com>
2024-10-23 11:41:44 +0100

    Initial Commit

---
+++ SYNC/SYNCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SYNCApp.swift
+//  SYNC
+//
+//  Created by Marcus Tiffen (CODING) on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SYNCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SYNCTests/SYNCTests.swift
@@ -0,0 +1,17 @@
+//
+//  SYNCTests.swift
+//  SYNCTests
+//
+//  Created by Marcus Tiffen (CODING) on 23/10/2024.
+//
+
+import Testing
+@testable import SYNC
+
+struct SYNCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
