691f1a44da251e7d594ce496e1ee218e374c6aae
Marcus Tiffen <marcustiffen6@gmail.com>
2025-11-12 10:04:07 +1100

    Improved flow with staging and release schemes. Fixed up ui on filter, create activity and activity filter views.

---
+++ SYNC/ObjectStructs/GroupChatRoom.swift
@@ -0,0 +1,8 @@
+//
+//  GroupChatRoom.swift
+//  SYNC
+//
+//  Created by Marcus Tiffen (CODING) on 11/11/2025.
+//
+
+import Foundation
---
+++ SYNC/Views/PostSignUp/Chat/CreateGroupChat/CreateGroupChatView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateGroupChatView.swift
+//  SYNC
+//
+//  Created by Marcus Tiffen (CODING) on 10/11/2025.
+//
+
+import SwiftUI
+
+struct CreateGroupChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateGroupChatView()
+}
